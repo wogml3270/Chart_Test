613c7b72d9e5592dcac448624ac387319693f82a
wogml3270 <wogml3270@naver.com>
2023-01-22 01:32:19 +0900

    pjh

--- test_app/test/src/Charts/PieChart.jsx
+++ test_app/test/src/Charts/PieChart.jsx
@@ -14,62 +14,83 @@ const PieChart = ({ data, ...props }) => {
   };
 
   return (
-    <div style={{ 
-      width: '457px', 
-      height: '300px', 
-      margin: '0 auto',
-      }}>
-      <ResponsivePie
-          {...props}
-          data={data}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          innerRadius={0.65}
-          padAngle={3}
-          cornerRadius={5}
-          activeOuterRadiusOffset={8}
-          borderWidth={1}
-          borderColor={{
-              from: 'color',
-              modifiers: [
-                  [ 'darker', 0 ]
-              ]
-          }}
-          colors={['#5E81F4', '#FF8282']}
-          arcLinkLabelsSkipAngle={10}
-          arcLinkLabelsTextColor="#333333"
-          arcLinkLabelsThickness={2}
-          arcLabelsSkipAngle={10000}
-          onClick={handle.pieClick}
-
-          legends={[
-              {
-                  anchor: 'bottom',
-                  direction: 'row',
-                  justify: false,
-                  translateX: -120,
-                  translateY: 65,
-                  itemsSpacing: 5,
-                  itemWidth: 80,
-                  itemHeight: 25,
-                  itemTextColor: '#000000',
-                  itemBackground: '#EEEEEE',
-                  itemDirection: 'left-to-right',
-                  itemOpacity: 1,
-                  symbolSize: 16,
-                  symbolShape: 'circle',
-                  effects: [
-                      {
-                          on: 'hover',
-                          style: {
-                            itemOpacity: 0.75
-                          }
-                      }
-                  ],
-                  onClick: handle.legendClick,
+    <>
+      <div style={{ 
+        width: '457px', 
+        height: '300px', 
+        margin: '0 auto',
+        position: 'relative',
+        }}>
+          <div style={{
+            color: '#000000',
+            fontSize: '16px',
+            fontWeight: 'bold',
+            position: 'absolute',
+            top: 10,
+            left: 20,
+            }}>
+              성별 통계
+          </div>
+        <ResponsivePie
+            {...props}
+            data={data}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.65}
+            padAngle={3}
+            cornerRadius={5}
+            activeOuterRadiusOffset={8}
+            borderWidth={1}
+            borderColor={{
+                from: 'color',
+                modifiers: [
+                    [ 'darker', 0 ]
+                ]
+            }}
+            theme={{
+              legends: {
+                text: {
+                  fontSize: 10,
+                  fill: '#000000',
+                },
               },
-          ]}
-      />
-    </div>
+            }}
+            colors={['#5E81F4', '#FF8282']}
+            arcLinkLabelsSkipAngle={false}
+            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsThickness={2}
+            arcLabelsSkipAngle={99999}
+            onClick={handle.pieClick}
+
+            legends={[
+                {
+                    anchor: 'bottom',
+                    direction: 'row',
+                    justify: false,
+                    translateX: -120,
+                    translateY: 65,
+                    itemsSpacing: 5,
+                    itemWidth: 80,
+                    itemHeight: 25,
+                    itemTextColor: '#000000',
+                    itemBackground: '#EEEEEE',
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 1,
+                    symbolSize: 16,
+                    symbolShape: 'circle',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                              itemOpacity: 0.75
+                            }
+                        }
+                    ],
+                    onClick: handle.legendClick,
+                },
+            ]}
+        />
+      </div>
+    </>
   );
 };
 
