7d76d513011f1883ef12bfe3b478109d5bf935c0
wogml3270 <wogml3270@naver.com>
2023-01-25 00:27:04 +0900

    pjh

--- test_app/test/src/App.js
+++ test_app/test/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import BarChart from './Charts/BarChart';
 import { BarData } from './Charts/Data/BarData';
 
 import PieChart from './Charts/PieChart';
 import { PieData } from './Charts/Data/PieData';
+import { PieData2 } from './Charts/Data/PieData2';
 
 import LevelChart from './Charts/LevelChart';
 import { LevelData1, LevelData2 } from './Charts/Data/LevelData';
@@ -14,35 +15,42 @@ import { LineData } from './Charts/Data/LineData';
 
 function App() {
 
+  const [state, setState] = useState(false);
+
   useEffect(() => {
     // PieData value 합계
     const sum1 = PieData.reduce((acc, cur) => {
       return acc + cur.value;
     }, 0);
+
     // LevelData1 value 합계
-    const sum3 = LevelData1.reduce((acc, cur) => {
+    const sum2 = LevelData1.reduce((acc, cur) => {
       return acc + cur.value;
     }
     , 0);
+
     // LevelData2 value 합계
-    const sum4 = LevelData2.reduce((acc, cur) => {
+    const sum3 = LevelData2.reduce((acc, cur) => {
       return acc + cur.value;
     }
     , 0);
+
 
     // PieData value를 합계의 퍼센트 값으로 변경
     PieData.forEach((d) => {
         d.value = Math.round((d.value / sum1) * 100);
       }
     );
+
     // LevelData1 value를 합계의 퍼센트 값으로 변경
     LevelData1.forEach((d) => {
-        d.value = Math.round((d.value / sum3) * 100);
+        d.value = Math.round((d.value / sum2) * 100);
       }
     );
+
     // LevelData2 value를 합계의 퍼센트 값으로 변경
     LevelData2.forEach((d) => {
-        d.value = Math.round((d.value / sum4) * 100);
+        d.value = Math.round((d.value / sum3) * 100);
       }
     );
 
@@ -59,7 +67,7 @@ function App() {
         <div className="main">
           <div className="top">
             <LineChart data={LineData} />
-            <PieChart valueFormat={(v) => `${v}%`} data={PieData} />
+            <PieChart valueFormat={(v) => `${v}%`} data={state ? PieData : PieData2 } />
           </div>
           <div className="bottom">
             <BarChart valueFormat={(v) => `${v}명`} data={BarData} />
